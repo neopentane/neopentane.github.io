e05ac7eae19817b7529293e28785c1b3c45e583a
Shreeram Kelkar <shreeramkelkar7@gmail.com>
2020-05-16 18:56:35 +0530

    asdfasdf

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueRouter from "vue-router";
+import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
+Vue.use(VueRouter);
 
 new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
